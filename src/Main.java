6ec2da70377d8ed8cbf68e8641d393a0225b9c4b
Admin <v.i.p@bk.ru>
2024-06-11 10:43:15 +0300

    hw fixed it

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,17 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
         int newYear = 2000;
-        printIsleapYear(newYear);
+        printIsLeapYear(newYear);
         printValidTypeInfo(0, 2024);
-        colculateDeliveryDays(110);
+        calculateDeliveryDays(100);
     }
 
-    public static void printIsleapYear(int year) {
+    public static void printIsLeapYear(int year) {
         System.out.println("Задача-1");
         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-            System.out.println(year + "год -високосный год");
+            System.out.println(year + " год - високосный год");
         } else {
-            System.out.println(year + "- не является високосным");
+            System.out.println(year + " - не является високосным");
         }
 
     }
@@ -24,35 +24,37 @@ public class Main {
         int currentYear = LocalDate.now().getYear();
         String resultType;
         String resultStatus;
-        if (type == 1) {
+        if (type == 0) {
             resultType = "IOS";
-        } else {
+        } else
             resultType = "Android";
-            if (type < currentYear) {
+            if (year > currentYear) {
                 resultStatus = "облегченную";
             } else {
                 resultStatus = "";
             }
-            System.out.printf("Требуется установить %s версию приложения %s", resultStatus, resultType);
-        }
+            System.out.printf("Требуется установить %s версию приложения %s", resultStatus,resultType);
+
 
     }
 
-    public static int colculateDeliveryDays(int distance) {
+    public static int calculateDeliveryDays(int distance) {
         System.out.println();
         System.out.println("Задача-3");
         int resultDays = 1;
-        if (distance >= 20) {
+        if (distance <= 20) {
             resultDays++;
         }
-        if (distance >= 60) {
+        if (distance <= 60) {
             resultDays++;
+            System.out.println("Потребуется дней: "+ resultDays);
         }
         if (distance >= 100) {
             System.out.println("Доставки нет");
             return -1;
         }
         return resultDays;
+
 
     }
 }
