ed304f2002206952308c466951f8ccf6547e2cfc
Admin <v.i.p@bk.ru>
2024-06-13 10:44:15 +0300

    homeWork redone it 2

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,17 @@ public class Main {
     public static void main(String[] args) {
         int newYear = 1200;
         printIsLeapYear(newYear);
-        printValidTypeInfo(1, 2024);// все работает корректно
+        printValidTypeInfo(1, 2024);
         calculateDeliveryDays(100);
     }
 
     public static void printIsLeapYear(int year) {
         System.out.println();
         System.out.println("Задача-1");
-        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {// о каком символе идет речь? его сдесь нет
-            System.out.println(year + " год - високосный год");// о каком символе идет речь? его сдесь нет
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год - високосный год");
         } else {
-            System.out.println(year + " - не является високосным");// о каком символе идет речь? его сдесь нет
+            System.out.println(year + " - не является високосным");
         }
 
     }
@@ -24,35 +24,36 @@ public class Main {
         System.out.println();
         System.out.println("Задача-2");
         int currentYear = LocalDate.now().getYear();
-        String resultType;
-        String resultStatus;
+        String resultStatus = "";
+        String resultType = "Android";
         if (type == 1) {
             resultType = "IOS";
-        } else
-            resultType = "Android";
+        }
         if (year < currentYear) {
             resultStatus = "облегченную";
-        } else {
-            resultStatus = "";
         }
-        System.out.printf("Требуется установить %s версию приложения для %s", resultStatus, resultType);// вывод проверил все норм
-        System.out.println();
-    }
-
-    public static int calculateDeliveryDays(int km) {
-        System.out.println();
-        System.out.println("Задача-3");
-        if (km <= 20) {
-            System.out.println("Потребуется дней:1");
-        } else if (km > 20 && km < 60) {
-            System.out.println("Потребуется дней:2");
-        } else if (km >= 60 && km < 100) {
-            System.out.println("Потребуется дней:3");
-        } else if (km >= 100) {
-            System.out.println("доставки нет.");// все корректно
-        }
+        System.out.printf("Требуется установить %s версию приложения для %s", resultStatus, resultType);
         System.out.println();
 
-        return km;
+}
+
+
+public static int calculateDeliveryDays(int distance) {
+    System.out.println();
+    System.out.println("Задача-3");
+    int resultDays = 1;
+    if (distance >= 20) {
+        resultDays++;
     }
-}// весь код проверил все работает что нужно еще исправить?
+    if (distance >= 60) {
+        resultDays++;
+    }
+    if (distance >= 100) {
+        System.out.println("Доставки нет");
+        return -1;
+    }
+    return resultDays;
+}
+        }
+
+
